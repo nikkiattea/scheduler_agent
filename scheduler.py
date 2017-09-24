cb2529c6494f72ee11b2b55d89da0705dcfb5083
Nicole Attea <nikkiattea@gmail.com>
2017-09-24 14:46:33 -0500

    Documentation for scheduler functions added
    Defines each function in scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -10,6 +10,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 sock.listen(5)
 
+"""
+Handles the outgoing TaskRequests and incoming
+TaskResults from the agent. Concurrent executions
+are not permitted, as per requirements.
+"""
 def handler(agent, json_data, outfile):
     print 'Recieved: %s' % agent
     json_result=[]
@@ -22,6 +27,12 @@ def handler(agent, json_data, outfile):
     writeResults(json_result, outfile)
     agent.close()
 
+"""
+Writes to a new line delimited json file per
+the json_result dictionary
+
+Default file: output.json
+"""
 def writeResults(json_result, filename):
     if filename is None:
         filename = "output.json"
@@ -30,6 +41,12 @@ def writeResults(json_result, filename):
             outfile.write(json.dumps(line)+"\n")
         outfile.close()
 
+"""
+Reads from a new line delimited json file and inserts
+those lines into a json dictionary
+
+Default file: input.json
+"""
 def readRequests(filename):
     json_data=[]
     if filename is None:
@@ -41,6 +58,22 @@ def readRequests(filename):
         infile.close()
     return json_data
 
+"""
+Parses command line arguments from scheduler.py
+
+usage: scheduler.py [-h] [--infile INFILE] [--outfile OUTFILE]
+
+Starts the scheduler service accepting an optional input and output file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --infile INFILE, -i INFILE
+                        path of the input file, default is input.json ex:
+                        input.json
+  --outfile OUTFILE, -o OUTFILE
+                        path of the out file, default is out.json ex:
+                        output.json
+"""
 def getOptions():
     parser = argparse.ArgumentParser(description='Starts the scheduler service accepting an optional input and output file')
     parser.add_argument('--infile', '-i', dest='infile', help='path of the input file, default is input.json ex: input.json')
@@ -48,6 +81,14 @@ def getOptions():
     options = parser.parse_args()
     return options
 
+"""
+Parses command line arguments and json input.
+Scheduler will listen on TCP port 3000 until a
+connection with an agent is established. It will 
+process all TaskRequests from the input file and
+return all TaskResults to the output file through
+the agent handler.
+"""
 def main():
     options = getOptions()
     json_data = readRequests(options.infile)
